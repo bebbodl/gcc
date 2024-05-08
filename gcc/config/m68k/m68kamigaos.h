bff87c54591e0bdcf5765e0604b4ba62d42d6ebb
bebbo <bebbo@bejy.net>
2024-05-08 12:16:59 +0200

    refs https://github.com/bebbo/libnix/issues/76: use fpucheck if 68881

--- gcc/config/m68k/m68kamigaos.h
+++ gcc/config/m68k/m68kamigaos.h
@@ -375,7 +375,10 @@ if (target_flags & (MASK_RESTORE_A4|MASK_ALWAYS_RESTORE_A4)) \
     "%{resident32:nlrcrt0.o%s}" \
     "%{!resident32:%{fbaserel32:nlbcrt0.o%s}" \
     "%{!fbaserel32:" \
-    "%{!mcpu=68000:%{!mcpu=68010:-u___cpucheck }} "\
+    "%{!mcpu=68000:%{!mcpu=68010:"\
+      "%{!m68881:-u___cpucheck }"\
+      "%{m68881:-u___fpucheck }"\
+    "}} "\
     "%{resident:nrcrt0.o%s}" \
     "%{!resident:%{fbaserel:nbcrt0.o%s}" \
     "%{!fbaserel:ncrt0.o%s}}}}}"
